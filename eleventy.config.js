e41e11c45a76588e042ab3247e81e7ac56e0a2bd
Niklas Graeber <dev@n1klas.net>
2024-11-19 23:57:38 +0100

    Transform to working Eleventy project

---
+++ eleventy.config.js
@@ -0,0 +1,50 @@
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+
+const viteOptions = {
+  publicDir: "public",
+  clearScreen: false,
+  server: {
+    middlewareMode: true
+  },
+  appType: "custom",
+  assetsInclude: ["**/*.xml", "**/*.txt"],
+  build: {
+    mode: "production",
+    sourcemap: "true",
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/css/main.[hash].css",
+        chunkFileNames: "assets/js/[name].[hash].js",
+        entryFileNames: "assets/js/[name].[hash].js",
+      },
+      plugins: [],
+    },
+  },
+};
+
+export default async function(eleventyConfig) {
+  eleventyConfig.setServerPassthroughCopyBehavior("copy");
+  eleventyConfig.addPassthroughCopy("public");
+
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    tempFolderName: "_tmp",
+    viteOptions,
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets/css");
+  eleventyConfig.addPassthroughCopy("src/assets/js");
+
+  return {
+    templateFormats: ["md", "njk", "html"],
+    htmlTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "includes",
+      layouts: "layouts",
+      data: "data",
+    }
+  };
+};
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+export default {
+  content: ["./src/**/*.{html,js,md,njk}"],
+  darkMode: "class",
+  theme: {
+    extend:
+      {
+        fontFamily: {
+          'nothingyoucoulddo': ['Nothing You Could Do', 'cursive'],
+          'signika': ['Signika', 'sans-serif'],
+        },
+      },
+  },
+  plugins: [],
+}
