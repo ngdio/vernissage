a192ec81383e7327ad7529b8b7a729b1b7083b18
Niklas Graeber <dev@n1klas.net>
2025-07-05 08:26:41 +0200

    Transform index page into reusable and dynamic grid layout

---
+++ src/layouts/grid.11ty.tsx
@@ -0,0 +1,43 @@
+import { BaseLayout } from "./base.11ty";
+import { Image } from "../includes/image"
+import { ViewProps } from "../props"
+
+export type GridPageImage = {
+  path: string
+  width: number
+  height: number
+  alt: string
+  rows?: number | 1
+  cols?: number | 1
+}
+
+export interface GridPageProps extends ViewProps {
+  images: GridPageImage[]
+}
+
+export function GridPage({ page, images }: GridPageProps): JSX.Element {
+  return (
+    <BaseLayout page={page}>
+      <script type="module" src="/assets/js/lightbox.js"></script>
+      <h1 class="text-4xl pt-10 pb-8 font-bold">PORTFOLIO</h1>
+      <section class="text-neutral-700">
+        <div class="container w-full">
+          <div class="grid grid-cols-4 gap-2 auto-rows-[90px] w-full" id="gallery-index">
+            {images.map(img => {
+              let className = "overflow-hidden";
+              if (img.rows && img.rows > 1) className += ` row-span-${img.rows}`;
+              if (img.cols && img.cols > 1) className += ` col-span-${img.cols}`;
+              return (
+                <div class={className}>
+                  <Image path={img.path} width={img.width+''} height={img.height+''} alt={img.alt} />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </section>
+    </BaseLayout>
+  )
+}
+
+export const render = GridPage
